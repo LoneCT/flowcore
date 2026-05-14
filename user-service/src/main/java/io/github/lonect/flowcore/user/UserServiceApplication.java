65071fad2dd01117c0633a88d5b3ba548b2fd02f
LoneCT <wwheng012@gmail.com>
2026-05-14 20:48:25 +0800

    feat: set up core configuration for user-service

--- user-service/src/main/java/io/github/lonect/flowcore/user/UserServiceApplication.java
+++ user-service/src/main/java/io/github/lonect/flowcore/user/UserServiceApplication.java
@@ -2,8 +2,15 @@ package io.github.lonect.flowcore.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        SecurityFilterAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class
+})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
